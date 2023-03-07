f0e5177f1329c91f13f0974cd37411d85aaf2eff
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-07 14:51:47 +0000

    cleanup and started article component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import Article from "./components/Article";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState("webdev");
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json").then((result) => {
+      if (result.status !== 200) {
+        console.log("ERROR");
+        return;
+      }
+
+      result.json().then((data) => {
+        if (data !== null) {
+          setArticles(data.data.children);
+        }
+      });
+    });
+  }, [subreddit]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" className="input" value="webdev" />
       </header>
+      <div className="articles">
+        <Article />
+      </div>
     </div>
   );
 }
